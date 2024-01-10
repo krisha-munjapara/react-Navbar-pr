d3644cafa3d14c50301aa0aad34f07490983056c
krisha-munjapara <135567252+krisha-munjapara@users.noreply.github.com>
2024-01-10 10:09:13 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+// import logo from './logo.svg';
+import './App.css';
+
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Navbar';
+// import Section from './Section';
+
+function App() {
+  return (
+   <>
+     <Navbar/>
+    {/* <Section/> */}
+    </>
+  );
+}
+export default App;
+
